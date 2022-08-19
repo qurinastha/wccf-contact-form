678eb25a3a0c078652fb57c52e60d4b9ca160541
qstha <qurina.shrestha@globdig.com>
2022-08-20 01:13:51 +0545

    First Step - Create Plugin

---
+++ wccf-contact-form.php
@@ -0,0 +1,17 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
+
+/**
+ * Plugin Name:       WC Contact Form
+ * Plugin URI:        https://github.com/qurinastha/wccf-contact-form
+ * Description:       A simple contact form plugin for the WordPress Plugin Development Workshop
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      5.0
+ * Author:            Qurina Shrestha
+ * Author URI:        https://github.com/qurinastha
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wccf-contact-form
+ * Domain Path:       /languages
+ */
