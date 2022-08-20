b91dda69586415a58cc6ca400ec293fc979c2f6e
qstha <qurina.shrestha@globdig.com>
2022-08-20 16:26:57 +0545

    Save Backend Form Settings

--- includes/backend/settings.php
+++ includes/backend/settings.php
@@ -1,12 +1,34 @@
-<?php defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );?>
+<?php defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
+$wccf_settings = get_option( 'wccf_settings' );
+if ( !empty( $wccf_settings ) ) {
+    $this->displayArr( $wccf_settings );
+}
+?>
 <div class="wrap">
      <div class="wccf-header">
          <h2><?php echo WCCF_PLUGINNAME;?> Settings</h2>
      </div>
+     <?php
+        if ( !empty( $_GET['message'] ) && $_GET['message'] == 1 ) {
+            ?>
+            <div class="notice notice-info is-dismissible inline">
+                <p>
+                    Settings saved successfully.
+                </p>
+            </div>
+            <?php
+        }
+    ?>
     <div class="wccf-settings-wrap">
+
         <em>Version: <?php echo WCCF_VERSION;?></em>
-        <form>
+
+        <form method="post" action="<?php echo admin_url( 'admin-post.php' ); ?>">
+
+             <input type="hidden" name="action" value="wccf_settings_save_action"/>
+
             <h3>Field Settings</h3>
+
             <div class="wccf-field-wrap">
                 <label>Name Field Label</label>
                 <div class="wccf-field">
@@ -41,7 +63,7 @@
             <div class="wccf-field-wrap">
                 <label></label>
                 <div class="wccf-field">
-                    <input type="button" class="btn button-primary" value="Save Settings"/>
+                    <input type="submit" class="btn button-primary" value="Save Settings"/>
                 </div>
             </div>
         </form>
--- wccf-contact-form.php
+++ wccf-contact-form.php
@@ -5,7 +5,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
  * Plugin Name:       WC Contact Form
  * Plugin URI:        https://github.com/qurinastha/wccf-contact-form
  * Description:       A simple contact form plugin for the WordPress Plugin Development Workshop
- * Version:           1.0.5
+ * Version:           1.0.6
  * Requires at least: 5.2
  * Requires PHP:      5.0
  * Author:            Qurina Shrestha
@@ -30,6 +30,8 @@ if ( !class_exists( 'WCCF_Contact_Form' ) ) {
             // admin enqueue scripts
             add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_assets' ) );
 
+            add_action( 'admin_post_wccf_settings_save_action', array( $this, 'save_settings_action' ) );
+
         }
 
 
@@ -38,7 +40,7 @@ if ( !class_exists( 'WCCF_Contact_Form' ) ) {
             defined( 'WCCF_PLUGINNAME' ) or define( 'WCCF_PLUGINNAME', 'WC Contact Form' );
             defined( 'WCCF_PATH' ) or define( 'WCCF_PATH', plugin_dir_path( __FILE__ ) );
             defined( 'WCCF_URL' ) or define( 'WCCF_URL', plugin_dir_url( __FILE__ ) );
-            defined( 'WCCF_VERSION' ) or define( 'WCCF_VERSION', '1.0.5' );
+            defined( 'WCCF_VERSION' ) or define( 'WCCF_VERSION', '1.0.6' );
 
         }
 
@@ -59,6 +61,38 @@ if ( !class_exists( 'WCCF_Contact_Form' ) ) {
             wp_enqueue_style( 'wccf-backend-style', WCCF_URL . 'assets/css/wccf-backend.css', array(), WCCF_VERSION );
             wp_enqueue_script( 'wccf-backend-script', WCCF_URL . 'assets/js/wccf-backend.js', array( 'jquery' ), WCCF_VERSION );
 
+        }
+
+        function save_settings_action(){
+
+            $name_field_label       = $_POST['name_field_label'];
+            $email_field_label      = $_POST['email_field_label'];
+            $message_field_label    = $_POST['message_field_label'];
+            $submit_button_label    = $_POST['submit_button_label'];
+            $admin_email            = $_POST['admin_email'];
+            
+            $wccf_settings = array(
+                'name_field_label'    => $name_field_label,
+                'email_field_label'   => $email_field_label,
+                'message_field_label' => $message_field_label,
+                'submit_button_label' => $submit_button_label,
+                'admin_email'         => $admin_email 
+            );
+
+            update_option( 'wccf_settings', $wccf_settings );
+
+            wp_redirect( admin_url( 'admin.php?page=wccf-contact-form&message=1' ) );
+
+            exit;
+
+        }
+
+        function displayArr( $array ) {
+            if ( isset( $_GET['debug'] ) ) {
+                echo "<pre>";
+                print_r( $array );
+                echo "</pre>";
+            }
         }
 
 
