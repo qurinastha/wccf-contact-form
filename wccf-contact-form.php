93fc7ffe4c9dcb435ee86133ed4dd2bed2f9014d
qstha <qurina.shrestha@globdig.com>
2022-08-20 16:01:04 +0545

    Define Constants and enqueue JS and CSS files

--- wccf-contact-form.php
+++ wccf-contact-form.php
@@ -5,7 +5,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
  * Plugin Name:       WC Contact Form
  * Plugin URI:        https://github.com/qurinastha/wccf-contact-form
  * Description:       A simple contact form plugin for the WordPress Plugin Development Workshop
- * Version:           1.0.3
+ * Version:           1.0.4
  * Requires at least: 5.2
  * Requires PHP:      5.0
  * Author:            Qurina Shrestha
@@ -22,7 +22,23 @@ if ( !class_exists( 'WCCF_Contact_Form' ) ) {
 
         function __construct() {
 
+            //define constants
+            $this->define_constants();
+
             add_action( 'admin_menu', array( $this, 'wccf_admin_menu' ) );
+
+            // admin enqueue scripts
+            add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_assets' ) );
+
+        }
+
+
+        function define_constants() {
+
+            defined( 'WCCF_PLUGINNAME' ) or define( 'WCCF_PLUGINNAME', 'WC Contact Form' );
+            defined( 'WCCF_PATH' ) or define( 'WCCF_PATH', plugin_dir_path( __FILE__ ) );
+            defined( 'WCCF_URL' ) or define( 'WCCF_URL', plugin_dir_url( __FILE__ ) );
+            defined( 'WCCF_VERSION' ) or define( 'WCCF_VERSION', '1.0.4' );
 
         }
 
@@ -34,9 +50,17 @@ if ( !class_exists( 'WCCF_Contact_Form' ) ) {
 
         function wccf_settings_page() {
 
-            include( plugin_dir_path( __FILE__ ) . 'includes/backend/settings.php' );
+            include( WCCF_PATH . 'includes/backend/settings.php' );
             
         }
+
+        function register_admin_assets(){
+
+            wp_enqueue_style( 'wccf-backend-style', WCCF_URL . 'assets/css/wccf-backend.css', array(), WCCF_VERSION );
+            wp_enqueue_script( 'wccf-backend-script', WCCF_URL . 'assets/js/wccf-backend.js', array( 'jquery' ), WCCF_VERSION );
+
+        }
+
 
     }
 
